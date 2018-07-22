098a099fde8633a47d20f2d0e26b838e82d1d8cc
AlexsaniKY <cerafem@gmail.com>
2018-07-22 15:26:14 -0400

    cleanup

--- src/sketches/zoomgridsketch.js
+++ src/sketches/zoomgridsketch.js
@@ -1,8 +1,5 @@
 import {grid, rainbowGrid} from "./helpers/draw/grid";
 
-
-
-
 function isMouseInside(p){
   if(
     p.mouseX >= 0 &&
@@ -15,13 +12,10 @@ function isMouseInside(p){
 }
 
 function mouseDelta(p){
-  let a = [p.mouseX - p.pmouseX, p.mouseY - p.pmouseY]
-  console.log(a);
-  return a;
+  return [p.mouseX - p.pmouseX, p.mouseY - p.pmouseY];
 }
 
 export default function ZoomGridSketch (p) {
-
   let scale = 1;
   let mouseIn = false;
   let isClicked = false;
@@ -30,7 +24,7 @@ export default function ZoomGridSketch (p) {
   let clickedY = 0;
   let draggedX = 0;
   let draggedY = 0;
-
+  let pts = [];
   let xOffset = 0;
   let yOffset = 0;
   let halfwidth = 0;
@@ -48,10 +42,6 @@ export default function ZoomGridSketch (p) {
       scale = props.scale * Math.PI / 180;
       scale = scale ? scale : 0.0001;
     }
-    // if (props.width && props.height){
-    //   p.resizeCanvas(props.width,props.height);
-    // }
-    
   };
 
   p.mouseWheel = (e) => {
@@ -65,16 +55,9 @@ export default function ZoomGridSketch (p) {
 
   p.mousePressed = () => {
     if(mouseIn){
-      //if(!isClicked){
         clickedX = p.mouseX;
         clickedY = p.mouseY;
         isClicked = true;
-      //}
-      // else{
-      //   draggedX = p.mouseX - p.pmouseX;
-      //   draggedY = p.mouseY - p.pmouseY;
-      //   console.log(draggedX + ", " + draggedY);
-      // }
       return false;
     }
     return true;
@@ -86,17 +69,7 @@ export default function ZoomGridSketch (p) {
     draggedY = 0;
   }
 
-  // p.mouseDragged = () => {
-  //   if(isClicked){
-  //     draggedX = p.mouseX - p.pmouseX;
-  //     draggedY = p.mouseY - p.pmouseY;
-  //     console.log(draggedX + ", " + draggedY);
-  //     return false;
-  //   }
-  //   return true;
-  // }
 
-  let pts = [];
   p.draw = () => {
     p.colorMode(p.RGB);
     p.background(100,0,150);
@@ -108,31 +81,31 @@ export default function ZoomGridSketch (p) {
     
     if(isClicked){
       [draggedX, draggedY] = mouseDelta(p);
+      if(!wasClicked){
+        pts.push((clickedX - halfheight)/scale - xOffset, (clickedY - halfwidth )/scale - yOffset);
+      }
     }
 
     xOffset += draggedX / scale;
     yOffset += draggedY / scale;
 
-    //pts.push((clickedX - halfheight)/scale - xOffset, (clickedY - halfwidth )/scale - yOffset);
+    
     
     
     p.push();
     p.translate(halfwidth, halfwidth);
-
-    
     p.scale(scale);
-    //p.translate(-halfwidth/scale, -halfheight/scale);
     p.translate(xOffset , yOffset );
     
     p.stroke(100,100,100);
     p.strokeWeight(4);
-    for (let i = 0; i<pts.length-2; i+=2){
+    for (let i = 0; i<pts.length-1; i+=2){
       p.point(pts[i],pts[i+1]);
     }
 
     rainbowGrid(p, {
        color: mouseIn? p.color(100,200,255) : p.color(100,100,100),
-       linewidth: 1, //isClicked? 2 : 1, 
+       linewidth: 1,
        cell:{x:10, y:10}, 
        dims:{x:100,y:100}
       });
