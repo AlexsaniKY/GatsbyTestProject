608cf019e3ff865318f6c7e52498e0a37caaedc9
AlexsaniKY <cerafem@gmail.com>
2018-07-30 18:46:48 -0400

    converted hardcoded portions to variables

--- src/sketches/zoomgridsketch.js
+++ src/sketches/zoomgridsketch.js
@@ -135,6 +135,7 @@ export default function ZoomGridSketch (p) {
     }
     p.pop();
     //pop into screenspace post-draw
+
     function* gridLineGen(origin, interval, start, end){
         let val = Math.ceil((start-origin)/interval)*interval + origin;
         while(val < end){
@@ -143,11 +144,18 @@ export default function ZoomGridSketch (p) {
         }
       }
 
-    let gridmin = Math.ceil(Math.log10(4/scale));
-    //console.log(gridmin);
-    for(let i=gridmin; i<gridmin+2; i++){
-      p.strokeWeight(i-gridmin + 1);
-      let spacing = Math.pow(10, i);
+    let gridpixelmin = 4;
+    let griddecade = 10;
+    let gridexponent = Math.log(gridpixelmin/scale) / Math.log(griddecade);
+    let gridminlevel = Math.max(Math.ceil(gridexponent), 0);
+    //add one to keep the lowest level from having reversed sign and undoing the 1- trick
+    //mod 1 to get a value between 0-1 for alpha
+    //subtract from 1 to reverse alpha fade, mult 255 to put in alpha range
+    let alpha = Math.pow(Math.abs(1 - ( (1 + gridexponent) % 1) ) , 2) * 255;
+    for(let i=gridminlevel; i<gridminlevel+2; i++){
+      p.strokeWeight((i-gridminlevel)*.1 + 1);
+      p.stroke(p.color(0,0,0, i == gridminlevel? alpha : 255));
+      let spacing = Math.pow(griddecade, i);
       for(let vert of gridLineGen(worldToScreen(0,0)[0], spacing*scale, 0, p.width)){
         p.line(
           vert, 
